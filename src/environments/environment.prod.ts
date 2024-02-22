964985e67f82e6a920fcf918650a18d341d941a5
Miguelmcanabal <miguelmcanabal@gmail.com>
2024-02-22 23:12:12 +0100

    minor fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ const realm = "comuneros"
 
 export const environment = {
     production: true,
-    apiUrl: "https://api-comuneros.miguelmontero.dev/comuneros/api/v1/",
+    apiUrl: "https://api-comuneros.miguelmontero.dev/comuneros/api/v1",
     authClientId: "comuneros-app",
     authIssuer: "https://auth-comuneros.miguelmontero.dev/realms/" + realm,
     webEndpoint: "https://comuneros.miguelmontero.dev",
